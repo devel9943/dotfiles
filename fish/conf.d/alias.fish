489ec3c8bcc039208a475825a5476f6eaebd0357
devel9943 <devel9943@proton.me>
2026-01-13 19:40:01 -0500

    migration to codeberg with pgp signing and ssh keys

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,1 +1,2 @@
 alias virsh="virsh -c qemu:///system "
+alias libreoffice="SAL_USE_VCLPLUGIN=gtk3 /usr/bin/libreoffice"
--- fish/config.fish
+++ fish/config.fish
@@ -6,5 +6,26 @@ function fish_greeting
     # smth smth
 end
 
+# wine isolated prefix function
+function wine-isolated
+    set app_name (basename $argv[1] .exe)
+    set prefix "$HOME/.wine-prefixes/$app_name"
+    
+    if not test -d "$prefix"
+        echo "Creating new Wine prefix: $prefix"
+        mkdir -p "$HOME/.wine-prefixes"
+    end
+    
+    env WINEPREFIX="$prefix" WINEUSERSANDBOX=1 wine $argv
+end
+
+# abbreviation for wine-isolated
+abbr -a wi wine-isolated
+
 # opencode
 fish_add_path /home/j/.opencode/bin
+
+# GPG SSH support
+set -gx GPG_TTY (tty)
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
