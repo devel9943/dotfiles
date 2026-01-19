370fa6e2388882da9f100394cc9363f38bee1efe
devel9943 <devel9943@proton.me>
2026-01-19 13:53:42 -0500

    added --git to ls.bash

--- bash/bash/alias/ls.bash
+++ bash/bash/alias/ls.bash
@@ -1,9 +1,9 @@
 # exa aliases
 # preferred listing
-alias ls='eza -lgF --colour=auto --icons=auto --group-directories-first'
+alias ls='eza -lgF --git --colour=auto --icons=auto --group-directories-first'
 # all files and dirs
-alias la='eza -algF --colour=auto --icons=auto --group-directories-first'
+alias la='eza -algF --git --colour=auto --icons=auto --group-directories-first'
 # tree listing
-alias lt='eza -aTg --colour=auto --icons=auto --group-directories-first'
+alias lt='eza -aTg --git --colour=auto --icons=auto --group-directories-first'
 # show only dotfiles
 alias l.="eza -ag | grep -e '^\.'"
